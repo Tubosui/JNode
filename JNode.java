4c239771b4673ad0e778c7b1b9eed18be3ca2168
= <=>
2015-03-08 15:26:19 +0900

    better

---
+++ JNode.java
@@ -0,0 +1,81 @@
+import java.util.List;
+
+/**
+ * 
+ * @author kazuma
+ * @param <T> JSONのvalue
+ */
+public class JNode<T>{
+  private String key;   //キー
+  private T val;        //値
+  private JNode<T> next;//次の要素
+  
+  /**
+   * keyとvalueをセットするコンストラクタ
+   * @param key　String型のkey
+   * @param val　value(型はString,Integer,JNodeHandler)
+   */
+  JNode(String key, T val){
+    this.key = key;
+    this.val = val;
+  }
+  
+  /**
+   * toString
+   * JSONのフォーマットにして返す(key:value)
+   */
+  @Override
+  public String toString(){
+    //keyの部分
+    String k = "\"" + this.key + "\":";
+    
+    /** valueの型によって処理をかえる **/
+    //Stringの場合、ダブルクウォートで囲って返す
+    if (this.val instanceof String) return k + "\"" + this.val + "\"";
+    
+    //JNodeHandlerの時、toStringメソッドを呼び出す
+    if (this.val instanceof JNodeHandler) return k + this.val.toString();
+    
+    //Listの時、[value1, value2, value3, .... , valueN]にして返す
+    if (this.val instanceof List){
+      StringBuffer sb = new StringBuffer();
+      sb.append(k +"[");
+      
+      /**Listの要素の方によって処理を変える**/
+      Object o = ((List) this.val).get(0);
+      int index = o instanceof String? 0 : 1;//フラグ(booleanにしなかったのは他に型が増えた時のため)
+      for(int i = 0; i<((List) this.val).size(); i++){
+        if(i != 0) sb.append(",");
+        
+        switch(index){
+        //Stringのとき
+        case 0 : sb.append("\"" + ((List) this.val).get(i) + "\"");
+                 break;
+        //String以外の時
+        case 1 : sb.append(((List) this.val).get(i).toString());
+        }
+      }
+      sb.append("]");
+      return sb.toString();
+    }
+    //String, JNodeHandler,List以外のとき(Integerなど)
+    else  return k + this.val;
+  }
+  
+  /**
+   * 次の要素を取得するメソッド(getter)
+   * @return JNode(次の要素)
+   */
+  public JNode<T> getNext() {
+    return next;
+  }
+
+  /**
+   * 渡されたJNodeを次の要素としてセットするメソッド(setter)
+   * @param next JNode(次の要素)
+   */
+  public void setNext(JNode<T> next) {
+    this.next = next;
+  }
+  
+}
---
+++ JNodeHandler.java
@@ -0,0 +1,57 @@
+
+/**
+ * JNodeのラッパークラス
+ * @author kazuma
+ * 片方向リストっぽい感じ
+ */
+public class JNodeHandler {
+  private JNode first; //最初の要素
+  private JNode last;  //最後の要素
+  
+  JNodeHandler(){
+  }
+  
+  /**
+   * JNodeを追加するメソッド
+   * @param key JSONのキーなる文字列
+   * @param val JSONのバリューとなる文字列
+   */
+  <T> void add(String key, T val){
+    //JNodeを生成して追加
+    JNode<T> n = new JNode<T>(key, val);
+    if(first == null){
+      this.first = n;
+      this.last = n;
+    } else {
+      this.last.setNext(n);
+      this.last = this.last.getNext();
+    }
+  }
+  
+  /**
+   * toStringメソッド
+   * JSON形式にして返す
+   */
+  @Override
+  public String toString(){
+    //空の場合は空文字を返す
+    if (this.first == null) return "";
+    
+    //最初の要素を取得
+    JNode n = this.first;
+    StringBuffer sb = new StringBuffer();
+    sb.append("{");
+    boolean flag = true;//最初だけ前に「,」をつけないようにするためのフラグ
+    while(n != null){
+      if (flag) flag = false;
+      else sb.append(",");
+      //JNodeクラスのtoString()メソッドを呼び出し、追加
+      sb.append(n.toString());
+      //次の要素へ(nullになるまで)
+      n = n.getNext();
+    }
+    sb.append("}");
+    return sb.toString();
+  }
+
+}
